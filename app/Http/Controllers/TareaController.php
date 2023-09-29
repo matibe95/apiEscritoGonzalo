29685790960d778cd64c1a56e484cd845685a54e
matibe95 <matipro365@gmail.com>
2023-09-29 09:24:30 -0300

    create function to create tasks

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -11,4 +11,29 @@ class TareaController extends Controller
     {
         return Tarea::all();
     }
+    public function crearTarea(Request $request)
+    {
+        try {
+            $Tarea = new Tarea();
+
+            $Tarea->titulo = $request->post('titulo');
+            $Tarea->contenido = $request->post('contenido');
+            $Tarea->estado = $request->post('estado');
+            $Tarea->autor = $request->post('autor');
+
+            $Tarea->save();
+            return $Tarea;
+        } catch (\Throwable $th) {
+            return ['message' => $th];
+        }
+    }
+    public function editarTarea(Request $request)
+    {
+        $Usuario = Tarea::findOrFail($request->post("id_user"));
+        // return Tarea::all();
+    }
+    public function eliminarTarea()
+    {
+        return Tarea::all();
+    }
 }
